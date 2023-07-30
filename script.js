95cab6884aefb105e19c50b605ff25a53f3c4c2d
hoangphuchvcx02 <137319917+hoangphuchvcx02@users.noreply.github.com>
2023-07-30 19:34:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -190,3 +190,42 @@ leButton = document.getElementById('toggleButton');
 	  }
 	});
 			
+	/**********************************************************************
+	 * 
+	 * 								SOFTSKILL AND ACTIVITIES
+	 * 
+	 *********************************************************************/
+	let slideIndex = 1;
+
+	showSlide(slideIndex);
+
+	function showSlide(n) {
+	const slides = document.getElementsByClassName("slide");
+	const dots = document.getElementsByClassName("dot");
+
+	if (n > slides.length) {
+		slideIndex = 1;
+	}
+	if (n < 1) {
+		slideIndex = slides.length;
+	}
+	
+	for (let i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	
+	for (let i = 0; i < dots.length; i++) {
+		dots[i].classList.remove("active");
+	}
+	
+	slides[slideIndex - 1].style.display = "block";
+	dots[slideIndex - 1].classList.add("active");
+	}
+
+	function currentSlide(n) {
+	showSlide(slideIndex = n);
+	}
+
+	function navigateSlides(n) {
+	showSlide(slideIndex += n);
+	}
